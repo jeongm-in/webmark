7595ba0bd1c63ef68ee60d0748a3c39572e205dd
Bartleby <bartleby2718@gmail.com>
2019-06-28 13:12:07 +0900

    Add two placeholder buttons close #1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './App.css';
+
+// const App: React.FC = () => {
+//   return (
+//     <div>
+//       <div>
+//         <button id="save">Save this web page</button>
+//       </div>
+//       <div>
+//         <button id="load">Load a random web page</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
