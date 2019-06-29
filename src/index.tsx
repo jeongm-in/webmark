ea0c743a6796904fe7ad59006630cfeb6dbccc56
Bartleby <bartleby2718@gmail.com>
2019-06-30 06:35:36 +0900

    Save web page to webmark folder close #12

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,26 @@ ReactDOM.render(
     document.getElementById('root') as HTMLElement
 );
 
+var saveClicked = (): void => {
+    chrome.storage.sync.get(
+        ['webmarkFolderId'],
+        function (result) {
+            if (Object.keys(result).length === 0) {
+                console.log('webmarkFolderId not found.');
+                createWebmarkFolder();
+            }
+            else {
+                console.log('webmarkFolderId found.');
+                let currentUrl: string = getCurrentUrl();
+                saveToWebmarkFolder(currentUrl);
+            };
+        }
+    );
+}
+
+let saveButton = document.getElementById('save') as HTMLElement;
+saveButton.addEventListener('click', saveClicked);
+
 // helper functions
 var createWebmarkFolder = (): void => {
     chrome.bookmarks.create(
@@ -31,6 +51,44 @@ var createWebmarkFolder = (): void => {
         },
     )
 };
+
+var getCurrentUrl = (): string => {
+    return 'https://current.url.com';
+};
+
+var saveToWebmarkFolder = (url: string): void => {
+    chrome.storage.sync.get(
+        ['webmarkFolderId'],
+        function (result?) {
+            if (Object.keys(result).length === 0) {
+                console.log('webmarkFolderId not found.');
+                createWebmarkFolder();
+            } else {
+                console.log('webmarkFolderId found.');
+                let webmarkFolderId: string = result!['webmarkFolderId'];
+                chrome.bookmarks.search(
+                    { 'url': url },
+                    function (results) {
+                        if (results === undefined || results.length == 0) {
+                            console.log('Same page not found in the folder.');
+                            chrome.bookmarks.create(
+                                {
+                                    'parentId': webmarkFolderId,
+                                    'url': url,
+                                }
+                            );
+                            console.log(url + ' saved to folder.');
+                        }
+                        else {
+                            showNotice('The page is already in the folder.');
+                        }
+                    }
+                )
+            }
+        }
+    );
+}
+
 var showNotice = (message: string): void => {
     console.log('Showed message ("' + message + '") to user.');
 }
