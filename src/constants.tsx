4416445e73ebf6cf188cb2e3c7fb35a1d9ddf419
Bartleby <bartleby2718@gmail.com>
2019-07-06 18:33:41 +0900

    Support i18n (English & Korean) close #54

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,3 +1,4 @@
+/* global chrome */
 /** Constant string literals in alphabetical order */
 // IDs and keys (user-invisible strings)
 export const FOLDER_ID_KEY: string = 'webmarkFolderId';
@@ -8,10 +9,10 @@ export enum NotificationId {
     SaveSuccessful = 'Save Successful',
 };
 
-// to be used for i18n later (user-visible strings)
-export const FOLDER_EMPTY: string = 'The folder is empty. Save some pages!';
-export const FOLDER_NAME: string = 'WebMark';
-export const LOAD_BUTTON_TEXT: string = 'Load a random web page';
-export const PAGE_ALREADY_EXISTS: string = 'The page is already in the folder.';
-export const SAVE_BUTTON_TEXT: string = 'Save this web page';
-export const SAVE_SUCCESSFUL: string = 'Page was saved successfully.';
+// user-visible strings
+export const FOLDER_EMPTY: string = chrome.i18n.getMessage('folderEmpty');
+export const FOLDER_NAME: string = chrome.i18n.getMessage('folderName');
+export const LOAD_BUTTON_TEXT: string = chrome.i18n.getMessage('loadButton');
+export const PAGE_ALREADY_EXISTS: string = chrome.i18n.getMessage('pageAlreadyExists');
+export const SAVE_BUTTON_TEXT: string = chrome.i18n.getMessage('saveButton');
+export const SAVE_SUCCESSFUL: string = chrome.i18n.getMessage('saveSuccessful');
